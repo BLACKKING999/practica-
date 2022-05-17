e972abcee62c1f81f271d1c78d771b0a4e469008
BLACKKING999 <jose.porozo.caicedo@utelvt.edu.ec>
2022-05-17 11:11:08 -0500

    PROGRAM c++ desde pc

---
+++ practica1/practica/holamundo.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int n;
+
+	cout << "ingrese numero:";
+	cin>>n;
+
+	cout<<"este es algo nuevo";
+
+	cout<<"usted ingreso este numero"<<n;
+
+	return 0;
+
+}
